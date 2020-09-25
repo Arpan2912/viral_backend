875fd4bc87fefcd0d101d6b80585409906ead657
Arpan2912 <shaharpan05@gmail.com>
2020-09-25 13:09:21 +0000

    update db data

--- db/index.js
+++ db/index.js
@@ -6,15 +6,20 @@ const db = {};
 
 // local
 console.log(process.env.DB_NAME, process.env.DB_USERNAME);
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USERNAME,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT
+//   }
+// );
+
+const sequelize = new Sequelize("diamond_tracker", "ACER", "123", {
+  host: "localhost",
+  dialect: "postgres"
+});
 
 sequelize
   .authenticate()
